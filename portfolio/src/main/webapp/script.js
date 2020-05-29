32118f3a787fb916224de97d09a150f351c776d8
Charlotte Hank <charlotte.hank@google.com>
2020-05-29 13:42:30 -0500

    added pictures

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I am on the Cheerleading team', 'I love ice cream', 'I like to sing', 'my favorite color is yellow'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
