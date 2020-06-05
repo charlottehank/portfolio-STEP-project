d3fff2300afd4f87a62c71b64cf6f369436b35bc
Charlotte Hank <charlotte.hank@google.com>
2020-06-05 20:05:32 +0000

    allow the user to choose the number of comments displayed

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -33,48 +33,33 @@ import java.util.Arrays;
 /** Servlet that returns content and comments*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    ArrayList<String> words = new ArrayList<String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //create query to store comments
     Query query = new Query("Task");
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
     
     //iterating over entities within Datastore
+    ArrayList<String> comments = new ArrayList<String>();
     for (Entity entity : results.asIterable()) {
-      long id = entity.getKey().getId();
       String title = (String) entity.getProperty("user-comments");
+      comments.add(title);
     }
 
+    //covert to json and send response
     response.setContentType("application/json;");
-
-    String json = "";
-
-    //get max number of comments
-    int numComments = getNumComments(request);
-
-    //convert request number of comments to json
-    if  (words.size() <= numComments){
-        json = new Gson().toJson(words);
-    }else{
-        for (int i = 0; i < numComments; i++){
-            json += new Gson().toJson(words.get(i));
-        }
-    }
-
-    response.getWriter().println(json);
-  }
+    Gson gson = new Gson();
+    response.getWriter().println(gson.toJson(comments));
+   }
 
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String userComment = request.getParameter("user-comments");
-
-    //add user comment to ArrayList
-    words.add(userComment);
 
     //entity with comment property to store user comments
     Entity taskEntity = new Entity("Task");
@@ -88,18 +73,19 @@ public class DataServlet extends HttpServlet {
     response.sendRedirect("/index.html");
   }
 
-/** Returns the choice entered by the player, or -1 if the choice was invalid. */
+
+  /** Returns the number of maximum comments */
   private int getNumComments(HttpServletRequest request) {
     // Get the input from the form.
     String numCommentsStr = request.getParameter("num-comments");
 
     // Convert the input to an int.
-    int numComments;
+    int numComments = 0;
     try {
       numComments = Integer.parseInt(numCommentsStr);
     } catch (NumberFormatException e) {
       System.err.println("Could not convert to int: " + numCommentsStr);
-      return -1;
+      return 1;
     }
 
     return numComments;
