701c54a5125143350ef8dfde6a0343f51523be5c
Charlotte Hank <charlotte.hank@google.com>
2020-06-05 20:20:22 +0000

    improved readability through clear comments and function cleanup

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -30,26 +30,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 
-/** Servlet that returns content and comments*/
+/** Servlet that returns comments*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //create query to store comments
+    //query for comment storage through datastore.
     Query query = new Query("Task");
-
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
     
-    //iterating over entities within Datastore
+    //populate comments ArrayList with entities within Datastore.
     ArrayList<String> comments = new ArrayList<String>();
     for (Entity entity : results.asIterable()) {
       String title = (String) entity.getProperty("user-comments");
       comments.add(title);
     }
 
-    //covert to json and send response
+    //covert comments to json for proper interpretation in script.js file.
     response.setContentType("application/json;");
     Gson gson = new Gson();
     response.getWriter().println(gson.toJson(comments));
@@ -58,37 +57,17 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the input from the form.
     String userComment = request.getParameter("user-comments");
 
-    //entity with comment property to store user comments
+    //create entity with comment property to store user comments.
     Entity taskEntity = new Entity("Task");
     taskEntity.setProperty("user-comments", userComment);
 
-    //storing using Datastore
+    //add comments to the Datastore for longterm storage. 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(taskEntity);
 
-    // Redirect back to the HTML page.
+    //send the user back to the HTML page after adding a comment.
     response.sendRedirect("/index.html");
   }
-
-
-  /** Returns the number of maximum comments */
-  private int getNumComments(HttpServletRequest request) {
-    // Get the input from the form.
-    String numCommentsStr = request.getParameter("num-comments");
-
-    // Convert the input to an int.
-    int numComments = 0;
-    try {
-      numComments = Integer.parseInt(numCommentsStr);
-    } catch (NumberFormatException e) {
-      System.err.println("Could not convert to int: " + numCommentsStr);
-      return 1;
-    }
-
-    return numComments;
-  }
-
 }
